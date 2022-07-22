c981abbf1a6f37ed52d28af1377e5ddd7140ef29
Kushagra Gupta <kushagra.gupta_cs20@gla.ac.in>
2022-07-22 10:30:11 +0530

    making folder of js and store files dynamically.

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Java is a type of :",
+    answer: "Object Oriented Programming Language",
+    options: [
+      "Object Based Programming Language",
+      "Object Oriented Programming Language",
+      "Object type Programming Language",
+      "Object Orientd Based Programming Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Java Compiler compiles the file(Source Code) in :",
+    answer: ".class extension",
+    options: [
+      ".object extension",
+      ".byte extension",
+      ".java extension",
+      ".class extension"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Unlike C++ java does not support?",
+    answer: "Operator Overloading",
+    options: [
+      "Reference Variable",
+      "Hierarchy level Inheritance",
+      "Operator Overloading",
+      "All Of These"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What happened when we create multiple objects in java which are not used?",
+    answer: "Garbage collector collect the objects and nothing happens",
+    options: [
+      "Java ignores them and move ahead",
+      "It throws an error",
+      "Garbage collector collect the objects and nothing happens",
+      "Java use interpreter"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Types Of arrays on the basis of size in java:",
+    answer: "Static array and Dynamic Array",
+    options: [
+      "Static array and Dynamic Array",
+      "Because Java arrays store in continuous memory so Static array",
+      "Java Might use Linked List but it has only Static Arrays",
+      "Java Supports Multidimensional arrays and so it is static"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Java Uses ____ for encoding the Characters ?",
+    answer: "UTF - 16 Unicode Transformation Format",
+    options: [
+      "ASCII - American Standard code for information interchange",
+      "UTF - 16 Unicode Transformation Format",
+      "Java is a high level programming language use UTF-16 and ASCII",
+      "Java does Not support UTF -16 because of 16 bits to store character"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Why Choose Java instead Of C++",
+    answer: "Java uses UTF-16 and have memory management,Exceptional Handling,Garbage collector",
+    options: [
+      "Java has support of backend development",
+      "Java Follow Object oriented Programming",
+      "Java uses UTF-16 and have memory management,Exceptional Handling,Garbage collector",
+      "Object Orientd Based Programming Language"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Strings In java are",
+    answer: "String Java, Both have Immutable as well Mutable String.",
+    options: [
+      "Mutable Generally",
+      "Immutable Generally ",
+      "Mutable Generally But we can make it by Using Constructors",
+      "String Java, Both have Immutable as well Mutable String."
+    ]
+  },
+  {
+    numb: 9,
+    question: "If we compare Two Strings in Java we use which of these to get desired output",
+    answer: "We should Use S1.compareTo() function",
+    options: [
+      "S1 == S2",
+      "S1.charAt(i)==S2.charAt(j)",
+      "We should Use S1.compareTo() function",
+      " Java Strings are stored in string pool so use Java S1===S2"
+    ]
+  },
+  {
+    numb: 10,
+    question: "what happens when we does not make a public class",
+    answer: "Error for not recognized",
+    options: [
+      "Nothing Happens, Program execute smoothly",
+      "Program might be crash",
+      "Exception may occured",
+      "Error for not recognized"
+    ]
+  },
+]
